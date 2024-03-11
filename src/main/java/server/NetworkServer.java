6625761f25453615b55caeff4d90ba84be8af54f
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-03-11 12:15:12 +0000

    Continued NetworkServer

--- src/main/java/server/NetworkServer.java
+++ src/main/java/server/NetworkServer.java
@@ -17,6 +17,9 @@ public class NetworkServer {
         try {
             server = new ServerSocket(port);
 
+            System.out.println("Server started on port " + port);
+            System.out.println("Waiting for clients...");
+
             while (true) {
                 Socket client = server.accept();
                 new Thread(() -> handleClient(client)).start();
@@ -28,7 +31,27 @@ public class NetworkServer {
     }
 
     private void handleClient(Socket clientSocket) {
-        // Handle Client Connectiosn
+        System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+        // Wait for the client to send a message and then send a response
+        try {
+            ObjectInputStream input = new ObjectInputStream(clientSocket.getInputStream());
+            ObjectOutputStream output = new ObjectOutputStream(clientSocket.getOutputStream());
+
+            while (true) {
+                String msg = (String) input.readObject();
+                System.out.println("Received: " + msg);
+
+                // Handle the message
+
+                output.writeObject("OK");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 
     private void close() {
