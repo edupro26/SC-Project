b3b725f364377be59baa80ebdb870acafc15d93c
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-03-11 12:14:10 +0000

    Made IoTServer main method

--- src/main/java/server/IoTServer.java
+++ src/main/java/server/IoTServer.java
@@ -1,4 +1,14 @@
 package server;
 
 public class IoTServer {
+  public static void main(String[] args) {
+
+        int port = args.length > 0 ? Integer.parseInt(args[0]) : 12345;
+
+        NetworkServer server = new NetworkServer(port);
+
+        server.start();
+
+
+    }
 }
