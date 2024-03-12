cc1709c113f3b83ed824a3a851f5234af7bf9e0a
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-03-12 16:13:56 +0000

    Created User class and Storage class for reading and writing data to files

---
+++ IoTServer/src/main/java/com/iot/server/Storage.java
@@ -0,0 +1,36 @@
+package com.iot.server;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Storage {
+    public static final String USERS_FILE = "users.txt";
+
+    public static User findUser(String name) {
+        Scanner scanner = new Scanner(USERS_FILE);
+
+        while (scanner.hasNextLine()) {
+            String[] parts = scanner.nextLine().split(":");
+            if (parts[0].equals(name)) {
+                return new User(parts[0], parts[1]);
+            }
+        }
+
+        return null;
+    }
+
+    public static void saveUser(User users) {
+        try {
+            FileWriter fileWriter = new FileWriter(USERS_FILE, true);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println(users);
+            printWriter.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
---
+++ IoTServer/src/main/java/com/iot/server/User.java
@@ -0,0 +1,37 @@
+package com.iot.server;
+
+import java.util.List;
+
+public class User {
+    private String name;
+
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+
+        Storage.saveUser(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static User findUser(String name) {
+        return Storage.findUser(name);
+
+    }
+
+    public String toString() {
+        return this.name + ":" + this.password;
+    }
+}
