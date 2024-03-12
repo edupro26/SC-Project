726c06341c1b9cd84498f28b863ee67d5cf18dde
edupro26 <edupro26@gmail.com>
2024-03-12 21:49:34 +0000

    Added login_data.csv to resources folder

--- IoTServer/src/main/java/com/iot/server/Storage.java
+++ IoTServer/src/main/java/com/iot/server/Storage.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Storage {
-    public static final String USERS_FILE = "users.txt";
+    public static final String USERS_FILE = "src/main/resources/login_data.csv";
 
     public static User findUser(String name) {
         Scanner scanner = new Scanner(USERS_FILE);
 
         while (scanner.hasNextLine()) {
-            String[] parts = scanner.nextLine().split(":");
+            String[] parts = scanner.nextLine().split(",");
             if (parts[0].equals(name)) {
                 return new User(parts[0], parts[1]);
             }
