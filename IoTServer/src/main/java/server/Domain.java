86ea6aa9c11705de55b038bda5e449759eaf35d1
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-03-24 22:40:42 +0000

    Made Domain javadoc

--- IoTServer/src/main/java/server/Domain.java
+++ IoTServer/src/main/java/server/Domain.java
@@ -8,13 +8,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
 
+/**
+ * Represents a domain in the server with its users and devices.
+ *
+ * @author Eduardo Proença - 57551, Manuel Barral - 52026, Tiago Oliveira - 54979
+ */
 public class Domain {
 
-    private final String name;
-    private final User owner;
-    private final List<User> users;
-    private final List<Device> devices;
+    private final String name; // domain name
+    private final User owner; // domain owner
+    private final List<User> users; // list of users with read permissions
+    private final List<Device> devices; // list of devices in the domain
 
+    /**
+     * Creates a new domain with a name and an owner.
+     * @param name
+     * @param owner
+     */
     protected Domain(String name, User owner) {
         this.name = name;
         this.owner = owner;
@@ -22,6 +32,11 @@ public class Domain {
         this.devices = new ArrayList<>();
     }
 
+    /**
+     * Creates a new domain given the representative string of the domain.
+     * @param domain the string representation of the domain
+     * @param srvStorage the storage of the server to get the users and devices
+     */
     protected Domain(String domain, Storage srvStorage) {
         String[] domainParts = domain.split(",");
         this.name = domainParts[0];
@@ -46,6 +61,10 @@ public class Domain {
         }
     }
 
+    /**
+     * Returns the file with the temperatures of the domain.
+     * @return the file with the temperatures of the domain.
+     */
     protected File getDomainTemperatures() {
         File file = new File( "temperatures/" + name + ".txt");
         try (BufferedWriter out = new BufferedWriter(new FileWriter(file, false))) {
@@ -63,26 +82,50 @@ public class Domain {
         return file.length() > 0 ? file : null;
     }
 
+    /**
+     * Adds a user to the domain.
+     * @param user the user to be added
+     */
     protected void addUser(User user) {
         users.add(user);
     }
 
+    /**
+     * Adds a device to the domain.
+     * @param device the device to be added
+     */
     protected void addDevice(Device device) {
         devices.add(device);
     }
 
+    /**
+     * Returns the name of the domain.
+     * @return the name of the domain
+     */
     protected String getName() {
         return name;
     }
 
+    /**
+     * Returns the owner of the domain.
+     * @return the owner of the domain
+     */
     protected User getOwner() {
         return owner;
     }
 
+    /**
+     * Returns the list of users with read permissions of the domain.
+     * @return list of users
+     */
     protected List<User> getUsers() {
         return users;
     }
 
+    /**
+     * Returns the list of devices in the domain.
+     * @return list of devices
+     */
     protected List<Device> getDevices() {
         return devices;
     }
