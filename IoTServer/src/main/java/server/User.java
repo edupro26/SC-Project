e48cb57d6db0fea22666ac2cc2f8b57bf47381ec
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-03-24 22:36:01 +0000

    Made User javadoc

--- IoTServer/src/main/java/server/User.java
+++ IoTServer/src/main/java/server/User.java
@@ -1,23 +1,46 @@
 package server;
 
+
+/**
+ * Represents a user with a name and a password.
+ *
+ * @author Eduardo Proença - 57551, Manuel Barral - 52026, Tiago Oliveira - 54979
+ */
 public class User {
 
     private final String name;
     private String password;
 
+    /**
+     * Creates a new user with a name and a password.
+     * @param name
+     * @param password
+     */
     public User(String name, String password) {
         this.name = name;
         this.password = password;
     }
 
+    /**
+     * Returns the name of the user.
+     * @return the name of the user
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the password of the user.
+     * @return the password of the user
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the password of the user.
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
