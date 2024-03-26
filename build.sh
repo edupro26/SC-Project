259703d895b26ee0d5b7c2de5d815dfde5a5c683
edupro26 <edupro26@gmail.com>
2024-03-26 14:02:13 +0000

    Updated build.sh and device_info.csv (Maven disabled)

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 # Build Server
 javac -d out/server/classes -cp IoTServer/src/main/java IoTServer/src/main/java/server/*.java
 cp -r IoTServer/src/main/resources/* out/server/classes
-jar cvfe out/IoTServer-grupo6.jar server.IoTServer -C out/server/classes .
+jar cvfe out/IoTServer.jar server.IoTServer -C out/server/classes .
 
 # Build Client
 javac -d out/client/classes -cp IoTDevice/src/main/java IoTDevice/src/main/java/client/*.java
-jar cvfe out/IoTDevice-grupo6.jar client.IoTDevice -C out/client/classes .
-
-
+jar cvfe out/IoTDevice.jar client.IoTDevice -C out/client/classes .
