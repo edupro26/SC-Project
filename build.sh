100475b33482b59962888ce72d719e54793d6b61
Eduardo Proença <edupro26@gmail.com>
2024-03-26 11:28:16 +0000

    Updated build.sh and created clean.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 # Build Server
-javac -d IoTServer/classes -cp IoTServer/src/main/java IoTServer/src/main/java/server/*.java
-cp -r IoTServer/src/main/resources/* IoTServer/classes
-jar cvfe IoTServer-grupo6.jar server.IoTServer -C IoTServer/classes .
+javac -d out/server/classes -cp IoTServer/src/main/java IoTServer/src/main/java/server/*.java
+cp -r IoTServer/src/main/resources/* out/server/classes
+jar cvfe out/IoTServer-grupo6.jar server.IoTServer -C out/server/classes .
 
 # Build Client
-javac -d IoTDevice/classes -cp IoTDevice/src/main/java IoTDevice/src/main/java/client/*.java
-jar cvfe IoTDevice-grupo6.jar client.IoTDevice -C IoTDevice/classes .
+javac -d out/client/classes -cp IoTDevice/src/main/java IoTDevice/src/main/java/client/*.java
+jar cvfe out/IoTDevice-grupo6.jar client.IoTDevice -C out/client/classes .
 
 
