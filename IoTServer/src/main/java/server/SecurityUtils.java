a68f29e0f2e72b720e9d9aa94925b426ff2c4596
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-04-06 16:29:33 +0100

    Created SecurityUtils for security related methods and made send2FA method

---
+++ IoTServer/src/main/java/server/SecurityUtils.java
@@ -0,0 +1,35 @@
+package server;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class SecurityUtils {
+    private static final String API_URL = "https://lmpinto.eu.pythonanywhere.com/2FA";
+    private static HttpClient client = HttpClient.newHttpClient();
+
+    /**
+     * Sends a 2FA code to the user's email.
+     *
+     * @param code the 2FA code to be sent
+     * @param email the email to which the code will be sent
+     * @param apiKey the API key to be used
+     * @return true if the code was sent successfully, false otherwise
+     */
+    public static boolean send2FACode(String code, String email, String apiKey) {
+        // Add parameters to the URL
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_URL + "?e=" + email + "&c=" + code + "&a=" + apiKey))
+                .build();
+
+        try {
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+}
