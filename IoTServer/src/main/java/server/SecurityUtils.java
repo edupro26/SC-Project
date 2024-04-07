59fa46c2113dd3517b1b88bec7cdcf336fc49681
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-04-07 01:00:56 +0100

    Created generateKey method for file encryption

--- IoTServer/src/main/java/server/SecurityUtils.java
+++ IoTServer/src/main/java/server/SecurityUtils.java
@@ -1,14 +1,43 @@
 package server;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class SecurityUtils {
     private static final String API_URL = "https://lmpinto.eu.pythonanywhere.com/2FA";
     private static HttpClient client = HttpClient.newHttpClient();
 
+    /**
+     * Generates a symmetric key given a cipher-password.
+     *
+     * @param cipherPassword the password to be used to generate the key
+     * @return the generated key
+     */
+    public static SecretKey generateKey(String cipherPassword) {
+        // TODO: Make salt secure
+        byte[] salt = { (byte) 0xc9, (byte) 0x36, (byte) 0x78, (byte) 0x99, (byte) 0x52, (byte) 0x3e, (byte) 0xea, (byte) 0xf2 };
+
+        PBEKeySpec keySpec = new PBEKeySpec(cipherPassword.toCharArray(), salt, 20);
+        try {
+            SecretKeyFactory kf = SecretKeyFactory.getInstance("PBEWithHmacSHA256AndAES_128");
+            return kf.generateSecret(keySpec);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
     /**
      * Sends a 2FA code to the user's email.
      *
