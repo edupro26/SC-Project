dd389d8ea22279da2ca0b9677d6891611327263c
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-04-09 14:52:35 +0100

    Made tests for signFile and verifySignature. All passing.

--- IoTServer/src/test/java/server/security/SecurityUtilsTest.java
+++ IoTServer/src/test/java/server/security/SecurityUtilsTest.java
@@ -1,9 +1,6 @@
 package server.security;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
 
@@ -13,6 +10,9 @@ class SecurityUtilsTest {
     private static final String dummyData = "INSERT_DUMMY_DATA_HERE";
     private static final String dummyFile = "hello.txt";
 
+    private static final String dummyFileToSign = "hello.txt";
+    private static final String dummySignedData = "DUMMY SIGNED DATA";
+
     @BeforeAll
     static void setUp() {
         File file = new File(dummyFile);
@@ -21,11 +21,13 @@ class SecurityUtilsTest {
         }
     }
 
+    @Order(1)
     @org.junit.jupiter.api.Test
     void generateKey() {
         assertNotNull(SecurityUtils.generateKey(CIPHER_PASSWORD));
     }
 
+    @Order(2)
     @org.junit.jupiter.api.Test
     @Disabled
     void send2FACode() {
@@ -36,14 +38,52 @@ class SecurityUtilsTest {
         assertTrue(result);
     }
 
+    @Order(3)
     @Test
     void encryptDataIntoFile() {
         SecurityUtils.encryptDataIntoFile(dummyData, new File(dummyFile), SecurityUtils.generateKey(CIPHER_PASSWORD));
     }
 
+    @Order(4)
     @Test
     void decryptDataFromFile() {
         SecurityUtils.encryptDataIntoFile(dummyData, new File(dummyFile), SecurityUtils.generateKey(CIPHER_PASSWORD));
         assertEquals(dummyData, SecurityUtils.decryptDataFromFile(new File(dummyFile), SecurityUtils.generateKey(CIPHER_PASSWORD)));
+    }
+
+    @Order(5)
+    @Test
+    void signFile() {
+        System.setProperty("javax.net.ssl.keyStore", "keystore.test");
+        System.setProperty("javax.net.ssl.keyStorePassword", "password");
+        SecurityUtils.signFile(new File(dummyFileToSign), dummySignedData);
+        assertTrue(new File(dummyFileToSign).exists());
+    }
+
+    @Order(6)
+    @Test
+    void verifySignature() {
+        System.setProperty("javax.net.ssl.keyStore", "keystore.test");
+        System.setProperty("javax.net.ssl.keyStorePassword", "password");
+        SecurityUtils.signFile(new File(dummyFileToSign), dummySignedData);
+        assertEquals(SecurityUtils.verifySignature(new File(dummyFileToSign)), dummySignedData);
+    }
+
+    @AfterAll
+    static void tearDown() {
+        // Delete the dummy file
+        File file = new File(dummyFile);
+        if (file.exists()) {
+            file.delete();
+        }
+
+        // Delete the signed file
+        File signedFile = new File(dummyFileToSign);
+        if (signedFile.exists()) {
+            signedFile.delete();
+        }
+
+
+
     }
 }
