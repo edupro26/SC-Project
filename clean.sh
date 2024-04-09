fb33bb65fa8eb2003efcab7281636b51a146dc26
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-04-09 01:53:10 +0100

    Added removes for the keystores and truststores

--- clean.sh
+++ clean.sh
@@ -1,1 +1,19 @@
 rm -rf out/
+
+# Remove server keystore
+rm -f keystore.server
+
+# Remove client 1 keystore
+rm -f keystore.user1
+
+# Remove client 1 truststore
+rm -f truststore.user1
+
+# Remove client 2 keystore
+rm -f keystore.user2
+
+# Remove client 2 truststore
+rm -f truststore.user2
+
+# Remove server certificate
+rm -f certServer.cer
