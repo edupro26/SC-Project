0a84086ef2970181a426b04d70352fee4b89155e
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-04-11 17:09:25 +0100

    Change truststore generation to one truststore per client.

--- clean.sh
+++ clean.sh
@@ -12,6 +12,9 @@ rm -f keystore.user1
 # Remove client 1 truststore
 rm -f truststore.user1
 
+# Remove clients truststore
+rm -f truststore.user
+
 # Remove client 2 keystore
 rm -f keystore.user2
 
@@ -20,3 +23,9 @@ rm -f truststore.user2
 
 # Remove server certificate
 rm -f certServer.cer
+
+# Remove client 1 certificate
+rm -f certClient1.cer
+
+# Remove client 2 certificate
+rm -f certClient2.cer
--- setupStores.sh
+++ setupStores.sh
@@ -14,7 +14,10 @@ keytool -exportcert -alias ServerKeyPair -file certServer.cer -keystore keystore
 keytool -genkeypair -alias user1 -keyalg RSA -keysize 2048 -keystore keystore.user1 -storepass alphatango -dname "CN=user1, OU=IT, O=MyCompany, L=MyCity, ST=MyState, C=PT" -validity 365
 
 # Setup Client 1 truststore with the server certificate
-keytool -importcert -alias ServerKeyPair -file certServer.cer -keystore truststore.user1 -storepass alphatango -noprompt
+keytool -importcert -alias ServerKeyPair -file certServer.cer -keystore truststore.user -storepass alphatango -noprompt
+
+#Export the client 1 certificate
+keytool -exportcert -alias user1 -file certClient1.cer -keystore keystore.user1 -storepass alphatango
 
 # --------------------------------------------
 
@@ -24,5 +27,16 @@ keytool -importcert -alias ServerKeyPair -file certServer.cer -keystore truststo
 keytool -genkeypair -alias user2 -keyalg RSA -keysize 2048 -keystore keystore.user2 -storepass alphatango -dname "CN=user2, OU=IT, O=MyCompany, L=MyCity, ST=MyState, C=PT" -validity 365
 
 # Setup Client 2 truststore
-keytool -importcert -alias ServerKeyPair -file certServer.cer -keystore truststore.user2 -storepass alphatango -noprompt
+keytool -importcert -alias ServerKeyPair -file certServer.cer -keystore truststore.user -storepass alphatango -noprompt
+
+# Export the client 2 certificate
+keytool -exportcert -alias user2 -file certClient2.cer -keystore keystore.user2 -storepass alphatango
+
+### IMPORT CLIENT CERTIFICATES ###
+
+# Import client 1 certificate into  truststore
+keytool -importcert -alias user1 -file certClient1.cer -keystore truststore.user -storepass alphatango -noprompt
+
+# Import client 2 certificate into truststore
+keytool -importcert -alias user2 -file certClient2.cer -keystore truststore.user -storepass alphatango -noprompt
 
