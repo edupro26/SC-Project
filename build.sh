07e04d3cc9f24d3f5a475e4a9a84a208fb0960b9
edupro26 <edupro26@gmail.com>
2024-04-17 23:46:28 +0100

    Created local client copy

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ jar cvfe server-app/IoTServer.jar server.IoTServer -C server-app/classes .
 javac -d client-app/classes -cp src/main/java src/main/java/common/*.java
 javac -d client-app/classes -cp src/main/java src/main/java/client/*.java
 jar cvfe client-app/IoTDevice.jar client.IoTDevice -C client-app/classes .
+
+# Store a client reference copy
+mkdir server-app/classes/client-copy
+cp client-app/IoTDevice.jar -d server-app/classes/client-copy
