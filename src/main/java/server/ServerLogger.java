52bf838eec7c92712aca08382050c7ffad17a8bb
edupro26 <edupro26@gmail.com>
2024-04-24 01:44:10 +0100

    Wrote javadoc

--- src/main/java/server/ServerLogger.java
+++ src/main/java/server/ServerLogger.java
@@ -2,21 +2,49 @@ package server;
 
 import java.util.logging.Logger;
 
-
+/**
+ * Simple logger utility class. Logs output
+ * messages from the server to the terminal
+ *
+ * @author Eduardo Proença (57551)
+ * @author Manuel Barral (52026)
+ * @author Tiago Oliveira (54979)
+ */
 public class ServerLogger {
 
+    /**
+     * Logger instance
+     */
     private static final Logger logger = Logger.getLogger("Logger");
 
+    /**
+     * Utility class not meant to be constructed
+     */
     private ServerLogger() {}
 
+    /**
+     * Log an info output message
+     *
+     * @param msg the message
+     */
     public static void logInfo(String msg) {
         logger.info(msg);
     }
 
+    /**
+     * Log a warning output message
+     *
+     * @param msg the message
+     */
     public static void logWarning(String msg) {
         logger.warning(msg);
     }
 
+    /**
+     * Log an error output message
+     *
+     * @param msg the message
+     */
     public static void logError(String msg) {
         logger.severe(msg);
     }
